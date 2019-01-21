66995938e6cff1b9748f06db1f9ad209a6445b37
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-21 18:14:54 +0000

    e

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -19,25 +19,18 @@
 const functions = require('./modules/functions.js');
 
 const init = () => {
-
- 
 	// init() gets executed only when the page is fully loaded
-
 	functions.setDaytimeMessage();
 	functions.setRandomBackground();
 	functions.setRandomQuote();
 	functions.setTime();
-
 	// set interval to update time every second
-
 	let timeInterval = setInterval(functions.setTime, 1000);
 };
 
 // initialize on page load through a listener
-
 document.addEventListener('DOMContentLoaded', init);
 
 // Disable right click
-
 document.oncontextmenu=RightMouseDown;
 function RightMouseDown() { return false; }
