0db9ea3e074a8f58cd1b347628bd54e38e4e2a8d
David Ralph <ohlookitsderpy@protonmail.com>
2019-06-26 13:14:06 +0100

    spaces are good

--- src/assets/js/modules/func.js
+++ src/assets/js/modules/func.js
@@ -26,18 +26,18 @@ const nal = navigator.language;
 
 module.exports = class Func {
     static setDaytimeMsg() {
-        if (util.contains.call(codes.itcodes, nal)) msg.itMsgSet(); //Italian
-        if (util.contains.call(codes.nlcodes, nal)) msg.nlMsgSet(); //Dutch
-        if (util.contains.call(codes.frcodes, nal)) msg.frMsgSet(); //French
-        //else if (util.contains.call(codes.ptcodes, nal)) Msg.ptMsgSet(); //Portuguese
-        if (util.contains.call(codes.spcodes, nal)) msg.spMsgSet(); //Spanish
-        if (util.contains.call(codes.ficodes, nal)) msg.fiMsgSet(); //Finnish
-        if (util.contains.call(codes.decodes, nal)) msg.spMsgSet(); //German
-        if (util.contains.call(codes.hecodes, nal)) msg.heMsgSet(); //Hebrew
-        if (util.contains.call(codes.rucodes, nal)) msg.ruMsgSet(); //Russian
-        if (util.contains.call(codes.arcodes, nal)) msg.arMsgSet(); //Arabic
-        if (util.contains.call(codes.svcodes, nal)) msg.svMsgSet(); //Swedish
-        else msg.engMsgSet(); //English
+        if (util.contains.call(codes.itcodes, nal)) msg.itMsgSet(); // Italian
+        if (util.contains.call(codes.nlcodes, nal)) msg.nlMsgSet(); // Dutch
+        if (util.contains.call(codes.frcodes, nal)) msg.frMsgSet(); // French
+        //else if (util.contains.call(codes.ptcodes, nal)) msg.ptMsgSet(); // Portuguese
+        if (util.contains.call(codes.spcodes, nal)) msg.spMsgSet(); // Spanish
+        if (util.contains.call(codes.ficodes, nal)) msg.fiMsgSet(); // Finnish
+        if (util.contains.call(codes.decodes, nal)) msg.spMsgSet(); // German
+        if (util.contains.call(codes.hecodes, nal)) msg.heMsgSet(); // Hebrew
+        if (util.contains.call(codes.rucodes, nal)) msg.ruMsgSet(); // Russian
+        if (util.contains.call(codes.arcodes, nal)) msg.arMsgSet(); // Arabic
+        if (util.contains.call(codes.svcodes, nal)) msg.svMsgSet(); // Swedish
+        else msg.engMsgSet(); // English
     }
 
     static setRandBg() {
@@ -46,10 +46,10 @@ module.exports = class Func {
 
     static setRandQuote() {
         const id = util.getRandIndex(quotes.authors);
-        if (util.contains.call(codes.itcodes, nal)) document.querySelector('blockquote').innerHTML = `"${quotes.it[id]}"`; //Italian
-        if (util.contains.call(codes.ptcodes, nal)) document.querySelector('blockquote').innerHTML = `"${quotes.pt[id]}"` || `"${quotes.eng[id]}"`; //Portuguese
-        if (util.contains.call(codes.spcodes, nal)) document.querySelector('blockquote').innerHTML = `"${quotes.sp[id]}"`; //Spanish
-        else document.querySelector('blockquote').innerHTML = `"${quotes.eng[id]}"`; //English
+        if (util.contains.call(codes.itcodes, nal)) document.querySelector('blockquote').innerHTML = `"${quotes.it[id]}"`; // Italian
+        if (util.contains.call(codes.ptcodes, nal)) document.querySelector('blockquote').innerHTML = `"${quotes.pt[id]}"` || `"${quotes.eng[id]}"`; // Portuguese
+        if (util.contains.call(codes.spcodes, nal)) document.querySelector('blockquote').innerHTML = `"${quotes.sp[id]}"`; // Spanish
+        else document.querySelector('blockquote').innerHTML = `"${quotes.eng[id]}"`; // English
         document.querySelector('cite').innerHTML = quotes.authors[id];
     }
 
@@ -60,8 +60,8 @@ module.exports = class Func {
                 util.formatTimeUnit(date.getMinutes()),
                 util.formatTimeUnit(date.getSeconds())
             ];
-        // joins all of the array elements into a string using the ':' separator
-        // example: [16, 32, 03] -> "16:32:03"
+        // Joins all of the array elements into a string using the ':' separator
+        // Example: [16, 32, 03] -> "16:32:03"
         document.querySelector('time').innerHTML = time.join(':');
     }
 };
