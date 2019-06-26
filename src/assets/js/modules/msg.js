a7c100667600f7ff048aec66af0860e27d7e1d6d
David Ralph <ohlookitsderpy@protonmail.com>
2019-06-26 13:19:43 +0100

    remove unneeded comments

--- src/assets/js/modules/msg.js
+++ src/assets/js/modules/msg.js
@@ -30,24 +30,24 @@ module.exports = class Msg {
     // Italian
     static itMsgSet() {
         let time = 'Buongiorno'; // Set the default time string
-        if (hour > 18) time = 'Buonasera'; // In Italian there is just Buongiorno or Buonasera
+        if (hour > 18) time = 'Buonasera'; // In Italian there is just Buongiorno or Buonasera, if it's before 6pm then set the time string to Buonasera
         document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
     }
 
     // Dutch
-    static nlMsgSet() {
-        let time = 'Goedemiddag'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'Goedemorgen'; //If it's before 12am, set the time string to "Good morning"
-        else if (hour > 18) time = 'Goedenavond'; //If it's before 6pm, set the time string to "Good afternoon"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+    static nlMsgSet() { // Everything below is the same as English, check the comments there for information
+        let time = 'Goedemiddag'; 
+        if (hour < 12) time = 'Goedemorgen'; 
+        else if (hour > 18) time = 'Goedenavond'; 
+        document.querySelector('.greeting').innerHTML = time; 
     }
 
     // French
     static frMsgSet() {
-        let time = 'Bonsoir'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'Bonjour'; //If it's before 12am, set the time string to "Good morning"
-        else if (hour > 18) time = 'Bonne après-midi'; //If it's before 6pm, set the time string to "Good afternoon"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+        let time = 'Bonsoir'; 
+        if (hour < 12) time = 'Bonjour'; 
+        else if (hour > 18) time = 'Bonne après-midi';
+        document.querySelector('.greeting').innerHTML = time; 
     }
 
     // Spanish
@@ -68,40 +68,40 @@ module.exports = class Msg {
 
     // German
     static deMsgSet() {
-        let time = 'Guten Abend'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'Guten Morgen'; // If it's before 12am, set the time string to "Good morning"
-        else if (hour > 18) time = 'Guten Nachmittag'; // If it's before 6pm, set the time string to "Good afternoon"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+        let time = 'Guten Abend'; 
+        if (hour < 12) time = 'Guten Morgen'; 
+        else if (hour > 18) time = 'Guten Nachmittag';
+        document.querySelector('.greeting').innerHTML = time;
     }
 
     // Hebrew
     static heMsgSet() {
-        let time = 'ערב טוב'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'בוקר טוב'; // If it's before 12am, set the time string to "Good morning"
-        else if (hour > 18) time = 'אחר הצהריים טובים'; // If it's before 6pm, set the time string to "Good afternoon"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+        let time = 'ערב טוב'; 
+        if (hour < 12) time = 'בוקר טוב';
+        else if (hour > 18) time = 'אחר הצהריים טובים';
+        document.querySelector('.greeting').innerHTML = time; 
     }
 
     // Russian
     static ruMsgSet() {
-        let time = 'Добрый Вечер'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'добрый утро'; // If it's before 12am, set the time string to "Good morning"
-        else if (hour > 18) time = 'добрый день'; // If it's before 6pm, set the time string to "Good afternoon"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+        let time = 'Добрый Вечер'; 
+        if (hour < 12) time = 'добрый утро'; 
+        else if (hour > 18) time = 'добрый день'; 
+        document.querySelector('.greeting').innerHTML = time; 
     }
 
     // Arabic
     static arMsgSet() {
-        let time = 'مساء الخير'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'صباح الخير'; // If it's before 12am, set the time string to "Good morning"
-        else if (hour > 18) time = 'مساء الخير'; // If it's before 6pm, set the time string to "Good afternoon"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+        let time = 'مساء الخير';
+        if (hour < 12) time = 'صباح الخير'; 
+        else if (hour > 18) time = 'مساء الخير'; 
+        document.querySelector('.greeting').innerHTML = time; 
     }
 
     // Swedish
     static svMsgSet() {
-        let time = 'God kväll'; // Set the default time string to "Good evening"
-        if (hour < 12) time = 'God morgon'; // If it's before 12am, set the time string to "Good morning"
-        document.querySelector('.greeting').innerHTML = time; // Write the string contents to the HTML
+        let time = 'God kväll'; 
+        if (hour < 12) time = 'God morgon';
+        document.querySelector('.greeting').innerHTML = time;
     }
 };
