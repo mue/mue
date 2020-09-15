a1963fb31b9602af5fa41d5eb047a4b3703ceae7
David Ralph <ohlookitsderpy@protonmail.com>
2020-09-15 12:40:20 +0100

    add it again but fixed possibly

--- src/components/modals/settings/Checkbox.jsx
+++ src/components/modals/settings/Checkbox.jsx
@@ -7,26 +7,24 @@ export default class Checkbox extends React.PureComponent {
   constructor(...args) {
     super(...args);
     this.state = {
-        checked: true
+      checked: true
     }
   }
 
+  handleChange(name) {
+    SettingsFunctions.setItem(name);
+    this.setState({ checked: (this.state.checked === true) ? false : true });
+  }
+
+  componentDidMount() {
+    this.setState({ checked: (localStorage.getItem(this.props.name) === 'true') });
+  }
+
   render() {
-    const handleChange = () => {
-      SettingsFunctions.setItem(this.props.name);
-      let checked;
-      if (this.state.checked === true) checked = false;
-      else checked = true;
-      this.setState({ checked: checked });
-    }
-
-    let value = localStorage.getItem(this.props.name);
-    this.setState({ checked: (value === 'true') });
-
     return (
       <React.Fragment>
         <FormControlLabel
-          control={<CheckboxUI name="checkedB" color="primary" checked={this.state.checked} onChange={handleChange} />}
+          control={<CheckboxUI name='checkedB' color='primary' checked={this.state.checked} onChange={() => this.handleChange(this.props.name)} />}
           label={this.props.text}
         />
         <br/>
