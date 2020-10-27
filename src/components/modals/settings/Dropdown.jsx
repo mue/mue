cc5fa542e150e9f5ef7e1edb3eae1cc8c0ecc997
Serhat <ss99@tuta.io>
2020-10-27 17:59:45 +0300

    Dropdowns are now standalone class

---
+++ src/components/modals/settings/Dropdown.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Dropdown extends React.PureComponent {
+  getLabel() {
+    return this.props.label ? <label htmlFor={this.props.name}>{this.props.label} </label> : null;
+  }
+
+  render() {
+    return (
+      <>
+        {this.getLabel()}
+        <div className='dropdown' style={{ display: 'inline' }}>
+          <select name={this.props.name} id={this.props.id} onChange={this.props.onChange}>
+            {this.props.children}
+          </select>
+        </div>
+      </>
+    );
+  }
+}
--- src/components/modals/settings/sections/LanguageSettings.jsx
+++ src/components/modals/settings/sections/LanguageSettings.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import Dropdown from '../Dropdown';
 
 export default class LanguageSettings extends React.PureComponent {
   render() {
     return (
-        <div className='section'>
+      <div className='section'>
         <h4 htmlFor='language' className='nodropdown'>{this.props.language.language}</h4>
-          <select className='select-css' name='language' id='language' onChange={() => localStorage.setItem('language', document.getElementById('language').value)}>
-            <option className='choices' value='en'>English</option>
-            <option className='choices' value='nl'>Nederlands</option>
-            <option className='choices' value='fr'>Français</option>
-            <option className='choices' value='no'>Norsk</option>
-            <option className='choices' value='ru'>Pусский</option>
-          </select>
-        </div>
+        <Dropdown
+          name='language'
+          id='language'
+          onChange={() => localStorage.setItem('language', document.getElementById('language').value)}
+        >
+          <option className='choices' value='en'>English</option>
+          <option className='choices' value='nl'>Nederlands</option>
+          <option className='choices' value='fr'>Français</option>
+          <option className='choices' value='no'>Norsk</option>
+          <option className='choices' value='ru'>Pусский</option>
+        </Dropdown>
+      </div>
     );
   }
 }
