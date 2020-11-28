87dd07c45c684cb41adb4b21bbb65d182403ee85
David Ralph <davetheprogrammer2@gmail.com>
2020-11-28 13:33:04 +0000

    actually fix it

--- src/components/modals/Welcome.jsx
+++ src/components/modals/Welcome.jsx
@@ -15,9 +15,9 @@ export default class Welcome extends React.PureComponent {
           <h2 className='subtitle'>{this.props.language.information}</h2>
           <p>{this.props.language.thankyoumessage1},<br/> {this.props.language.thankyoumessage2}</p>
           <h2 className='subtitle'>{this.props.language.support}</h2>
-          <a href='mailto:hello@muetab.com' className='welcomeLink' target='_blank' rel='noreferrer'><EmailIcon/></a>
-          <a href='https://twitter.com/getmue' className='welcomeLink' target='_blank' rel='noreferrer'><TwitterIcon/></a>
-          <a href='https://discord.gg/zv8C9F8' className='welcomeLink' target='_blank' rel='noreferrer'><ForumIcon/></a>
+          <a href='mailto:hello@muetab.com' className='welcomeLink' target='_blank' rel='noopener noreferrer'><EmailIcon/></a>
+          <a href='https://twitter.com/getmue' className='welcomeLink' target='_blank' rel='noopener noreferrer'><TwitterIcon/></a>
+          <a href='https://discord.gg/zv8C9F8' className='welcomeLink' target='_blank' rel='noopener noreferrer'><ForumIcon/></a>
           <br/>
           <button className='close' onClick={this.props.modalClose}>{this.props.language.close}</button>
         </div>
