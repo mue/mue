3fec08a492343eff8c9a80c53cc93c48fdd08381
David Ralph <davetheprogrammer2@gmail.com>
2020-12-06 12:55:42 +0000

    more cleanup and fixes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import './scss/index.scss';
+import 'react-clock/dist/Clock.css';
 import 'react-toastify/dist/ReactToastify.css'; // the toast css is based on default so we need to import it
 
 import 'fontsource-lexend-deca/latin-400-normal.css';
