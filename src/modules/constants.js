7990286e9ad27db28b89ce4a1cde32ef60d708e9
David Ralph <ohlookitsderpy@protonmail.com>
2021-06-21 17:45:06 +0100

    style: cleanup constants

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -1,16 +1,25 @@
+// API URLs
 export const API_URL = 'https://api.muetab.com';
 export const UNSPLASH_URL = 'https://unsplash.muetab.com';
 export const PEXELS_URL = 'https://pexels.muetab.com';
 export const MARKETPLACE_URL = 'https://marketplace.muetab.com';
 export const WEATHER_URL = 'https://weather.muetab.com';
-export const WEBSITE_URL = 'https://muetab.com';
 export const SPONSORS_URL = 'https://sponsors.muetab.com';
 export const GITHUB_URL = 'https://api.github.com';
+export const DDG_PROXY = 'https://external-content.duckduckgo.com/iu/?u=';
+
+// Mue URLs
+export const WEBSITE_URL = 'https://muetab.com';
 export const BLOG_POST = 'https://blog.muetab.com/posts/version-5-1';
 export const FEEDBACK_FORM = 'https://api.formcake.com/api/form/349b56cb-7e2b-4004-b32b-e8964d217dd1/submission';
-export const DDG_PROXY = 'https://external-content.duckduckgo.com/iu/?u=';
+
+// umami
 export const UMAMI_DOMAIN = 'https://umami.muetab.com';
 export const UMAMI_ID = '1b97e723-199c-48d8-8992-17c4e22d4f3c';
+
+// Offline images
 export const OFFLINE_IMAGES = 20;
+
+// Version
 export const BETA_VERSION = false;
 export const VERSION = '5.1.0'; 
