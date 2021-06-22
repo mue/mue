5d3418a8af4e00c80323ad34f0b7a67227d4f16c
David Ralph <ohlookitsderpy@protonmail.com>
2021-06-22 21:07:56 +0100

    build: optimise production builds, automate creating zip files

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,29 +3,45 @@ const common = require('./webpack.common.js');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const ZipFilePlugin = require('zip-file-webpack-plugin');
 
-module.exports = merge(common, {
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: '[name].[chunkhash].js',
-    chunkFilename: '[id].[chunkhash].chunk.js',
-    clean: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './public/index.html')
-    }),
-    new CopyPlugin({
-      patterns: [{
-          from: 'public/icons',
-          to: 'icons'
-        },
-        {
-          from: 'public/offline-images',
-          to: 'offline-images'
-        }
-      ]
-    })
-  ]
-});
+module.exports = (env) => {
+  const type = env.type || 'web';
+
+  return merge(common, {
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, './build'),
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[id].[chunkhash].chunk.js',
+      clean: true
+    },
+    cache: {
+      type: 'filesystem'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './public/index.html')
+      }),
+      new CopyPlugin({
+        patterns: [{
+            from: 'public/icons',
+            to: 'icons'
+          },
+          {
+            from: 'public/offline-images',
+            to: 'offline-images'
+          },
+          {
+            from: `manifest/${type}.json`,
+            to: 'manifest.json'
+          }
+        ]
+      }),
+      new ZipFilePlugin({
+        path: '../dist/',
+        filename: `${type}.zip`
+      })
+    ]
+  });
+}
