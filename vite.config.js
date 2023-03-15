b8eabc87173b2f3d66c3145ea8ec25dd93e9cce9
David Ralph <me@davidcralph.co.uk>
2023-03-15 11:20:01 +0000

    feat(dev): automate build zip creation

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,65 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import fs from 'fs';
+import ADMZip from 'adm-zip';
+import * as pkg from './package.json';
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const prepareBuilds = () => ({
+  name: 'prepareBuilds',
+  buildEnd() {
+    if (isProd) {
+      // chrome
+      fs.mkdirSync(path.resolve(__dirname, './build/chrome'), { recursive: true });
+      fs.copyFileSync(
+        path.resolve(__dirname, './manifest/chrome.json'),
+        path.resolve(__dirname, './build/chrome/manifest.json'),
+      );
+      fs.copyFileSync(
+        path.resolve(__dirname, './manifest/background-chrome.js'),
+        path.resolve(__dirname, './build/chrome/background.js'),
+      );
+      fs.cpSync(
+        path.resolve(__dirname, './manifest/_locales'),
+        path.resolve(__dirname, './build/chrome/_locales'),
+        {
+          recursive: true,
+        },
+      );
+      fs.cpSync(path.resolve(__dirname, './dist'), path.resolve(__dirname, './build/chrome/'), {
+        recursive: true,
+      });
+
+      // firefox
+      fs.mkdirSync(path.resolve(__dirname, './build/firefox'), { recursive: true });
+      fs.copyFileSync(
+        path.resolve(__dirname, './manifest/firefox.json'),
+        path.resolve(__dirname, './build/firefox/manifest.json'),
+      );
+      fs.copyFileSync(
+        path.resolve(__dirname, './manifest/background-firefox.js'),
+        path.resolve(__dirname, './build/firefox/background.js'),
+      );
+      fs.cpSync(path.resolve(__dirname, './dist'), path.resolve(__dirname, './build/firefox/'), {
+        recursive: true,
+      });
+
+      // create zip
+      const zip = new ADMZip();
+      zip.addLocalFolder(path.resolve(__dirname, './build/chrome'));
+      zip.writeZip(path.resolve(__dirname, `./build/chrome-${pkg.version}.zip`));
+
+      const zip2 = new ADMZip();
+      zip2.addLocalFolder(path.resolve(__dirname, './build/firefox'));
+      zip2.writeZip(path.resolve(__dirname, `./build/firefox-${pkg.version}.zip`));
+    }
+  },
+});
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), prepareBuilds()],
   server: {
     hmr: {
       protocol: 'ws',
