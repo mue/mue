e79a04cb3595e6f5efab06844b6e57cc107b0d9e
David Ralph <me@davidcralph.co.uk>
2023-03-15 00:05:22 +0000

    chore: change vite plugin to react swc

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 const isProd = process.env.NODE_ENV === 'production';
