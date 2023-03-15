259eab31c675f991968a82a90fdf14f029e67fe6
David Ralph <me@davidcralph.co.uk>
2023-03-15 20:46:56 +0000

    feat(dev): add remove unused translation string support to script

--- scripts/updatetranslations.js
+++ scripts/updatetranslations.js
@@ -11,7 +11,17 @@ fs.readdirSync('../src/translations').forEach((file) => {
     require('../src/translations/en_GB.json'),
     require('../src/translations/' + file),
   );
+
+  // remove unused strings
+  const en = require('../src/translations/en_GB.json');
+  const keys = Object.keys(newdata);
+  const enkeys = Object.keys(en);
+  const unused = keys.filter((key) => !enkeys.includes(key));
+  unused.forEach((key) => {
+    delete newdata[key];
+  });
   fs.writeFileSync('../src/translations/' + file, JSON.stringify(newdata, null, 2));
+
   // add new line
   fs.appendFileSync('../src/translations/' + file, '\n');
 });
