73bc89d7aed5164c7cbd123bd32d900ff4d4d4fe
David Ralph <me@davidcralph.co.uk>
2026-01-03 11:32:03 +0000

    chore: update translations url

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,13 +7,12 @@ export const OPENSTREETMAP_URL = 'https://www.openstreetmap.org';
 // Mue URLs
 export const WEBSITE_URL = 'https://muetab.com';
 export const PRIVACY_URL = 'https://muetab.com/privacy';
-export const TRANSLATIONS_URL = 'https://docs.muetab.com/translations/';
+export const TRANSLATIONS_URL = 'https://muetab.com/docs/translations';
 export const WEBLATE_URL = 'https://hosted.weblate.org/projects/mue/mue-tab/';
 export const REPORT_ITEM =
   'https://github.com/mue/marketplace/issues/new?assignees=&labels=item%2Creport&projects=&template=item_issue_report.yml&title=%5BItem+Report%5D+Insert+marketplace+item+name+here';
 export const BUG_REPORT =
   'https://github.com/mue/mue/issues/new?assignees=&labels=bug&projects=&template=issue_report.yml&title=%5BBug%5D+Insert+issue+name+here';
-export const DONATE_LINK = 'https://muetab.com/donate';
 export const SENTRY_DSN =
   'https://430352fd4b174d688ebd82fc85c22c58@o1217438.ingest.sentry.io/6359480';
 export const KNOWLEDGEBASE = 'https://support.muetab.com';
